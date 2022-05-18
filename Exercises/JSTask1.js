5171369adbba5d7247c1e733eb82e01280ad8cdd
Redy <redy@live.co.uk>
2022-05-18 10:43:43 +0100

    JS task 1 complete

---
+++ Exercises/JSTask1.js
@@ -0,0 +1,15 @@
+console.log("Richard");
+console.info("de Young");
+console.warn("Buckfastleigh");
+console.error("Sagittarius");
+
+const make = "Toyota";
+const model = "Corolla";
+
+console.log("My Favourite car is", make, "and the model is", model,".");
+
+console.log(`My Favourite car is ${make} and the model is ${model}.`);
+
+let extraText = "I'm sure this MAY be useful...?";
+
+console.log("This is where I will be writing a %c a random block of text with CSS styling added."+extraText,"color: orange; font-family: fantasy; font-style: bold; background-color: black;padding: 10px");
